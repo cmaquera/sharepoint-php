7d3af71c72c4e1d488d5f227fa757228c9dc40cc
Cesar Maquera <cmaquerat@gmail.com>
2023-08-07 23:17:00 +0000

    Inicializacion de proyecto

--- app.php
+++ app.php
@@ -1,1 +1,26 @@
+<?php
+    require_once './vendor/autoload.php';
+
+    use Office365\GraphServiceClient;
+    use Office365\Runtime\Auth\AADTokenProvider;
+    use Office365\Runtime\Auth\UserCredentials;
+
+
+    function acquireToken()
+    {
+        $tenant = ".onmicrosoft.com";
+        $resource = "https://graph.microsoft.com";
+
+        $provider = new AADTokenProvider($tenant);
+        return $provider->acquireTokenForPassword($resource, "{clientId}",
+            new UserCredentials("{UserName}", "{Password}"));
+    }
+
+    $client = new GraphServiceClient("acquireToken");
+    $drive = $client->getMe()->getDrive();
+    $client->load($drive);
+    $client->executeQuery();
+    print $drive->getWebUrl();
+
+?>
 
